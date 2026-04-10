cc3ff81b41ea968f3f3a1ff34b384e23d0e19b46
Your Name <your-email@gmail.com>
2026-04-10 19:32:21 +0530

    fix tsconfig and remove build errors

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-        I am a Class 10 student with a strong focus on academics and a growing passion for technology. I am also a state-level chess player, which has helped me develop strategic thinking, focus, and discipline. I am building my skills in HTML, CSS, JavaScript, and Python, and I enjoy learning new concepts and improving myself both academically and personally. My goal is to grow into a skilled developer while continuing to excel in my studies.
+        I am a Class 10 student with a strong focus on academics and a growing passion for technology. I am also a state-level chess player, which has helped me develop strategic thinking, focus, and discipline. I am building my skills in HTML, CSS, JavaScript, and Python, and I enjoy learning new concepts and improving myself both academically and personally. My goal is to grow into a skilled developer while continuing to develop more in my studies.
         </p>
       </div>
     </div>
